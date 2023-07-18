411391e2f0e9820d349312867d04b5909dba94f4
Raj kavankar <rajkavankar@gmail.com>
2023-07-18 11:15:37 +0530

    tailwind and asset setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        dartGrey: "hsl(234, 29%, 20%)",
+        regularGrey: "Grey: hsl(231, 7%, 60%)",
+      },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
